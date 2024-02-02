6c6aee3a34149f6eb0840d8545483adf4b57848d
murodjon2423 <azimovmurod226@gmail.com>
2024-02-02 17:56:27 +0500

    tugatdik

--- 5_yigindilar_soni.py
+++ 5_yigindilar_soni.py
@@ -1,12 +1,10 @@
 k = open("INPUT.txt", "r")
-f=k.readline().split()
+f=k.readline()
 h1=int(f)
 print(h1)
-if a==38:
-    oo= open("OUTPUT.txt", "a")
-    o1=int(s)*10+5
-    oo.write(str(o1))
-    oo.close()
-    print(int(s)*10+5)
-        
+oo= open("OUTPUT.txt", "a")
+o1=int(int(f)/2+1)
+oo.write(str(o1))
+oo.close()
+    
         
